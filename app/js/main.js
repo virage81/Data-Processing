a235a4bd20b757ae3175d6e4b134979cde4584d3
Bulat <karimovMINDS@gmail.com>
2022-06-08 05:35:02 +0400

    fixed js file

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,22 @@
 $(function () {
-	$btn = $("#header-btn");
-	$nav = $("#header-nav");
-	$links = $(".header__link");
+	let btn = $("#header-btn"),
+		nav = $("#header-nav"),
+		links = $(".header__link");
 
-	$nav.removeClass("show");
+	nav.removeClass("show");
 
-	$btn.on("click", () => {
-		$nav.addClass("show");
+	btn.on("click", () => {
+		nav.addClass("show");
 	});
 
 	$(window).on("click", function (event) {
 		if ($(event.target).is($("#header-nav"))) {
-			$nav.removeClass("show");
+			nav.removeClass("show");
 		}
 
-		for ($link of $links) {
-			if ($(event.target).is($link)) {
-				$nav.removeClass("show");
+		for (let link of links) {
+			if ($(event.target).is(link)) {
+				nav.removeClass("show");
 			}
 		}
 	});
